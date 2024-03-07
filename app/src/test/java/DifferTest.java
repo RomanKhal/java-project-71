ade827143b646d7930bd212bb20476e3cb4fc065
RomanKhal <haluev.roman@mail.ru>
2024-03-07 17:20:38 +0300

    fix 1st stage coach validation

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -16,32 +16,70 @@ public class DifferTest {
 
 
     @Test
-    public void testStylish() throws IOException {
-        String json1 = "src/test/resources/file1.json";
-        String json2 = "src/test/resources/file2.json";
-        String expected = Files.readString(Path.of("src/test/resources/result_stylish.txt"));
+    public void testJsonToStylish() throws IOException {
+        String json1 = "src/test/resources/fixtures/file1.json";
+        String json2 = "src/test/resources/fixtures/file2.json";
+        String expected = Files.readString(Path.of("src/test/resources/fixtures/result_stylish.txt"));
         assertEquals(expected, Differ.generate(json1, json2, "stylish"));
     }
 
     @Test
-    public void testPlain() throws IOException {
-        String json1 = "src/test/resources/file1.yml";
-        String json2 = "src/test/resources/file2.yml";
-        String expected = Files.readString(Path.of("src/test/resources/result_plain.txt"));
+    public void testJsonToPlain() throws IOException {
+        String json1 = "src/test/resources/fixtures/file1.json";
+        String json2 = "src/test/resources/fixtures/file2.json";
+        String expected = Files.readString(Path.of("src/test/resources/fixtures/result_plain.txt"));
         assertEquals(expected, Differ.generate(json1, json2, "plain"));
     }
 
     @Test
-    public void testJson() throws IOException {
-        String json1 = "src/test/resources/file1.yml";
-        String json2 = "src/test/resources/file2.yml";
-        String expected = Files.readString(Path.of("src/test/resources/result_json.txt"));
+    public void testJsonToJson() throws IOException {
+        String json1 = "src/test/resources/fixtures/file1.json";
+        String json2 = "src/test/resources/fixtures/file2.json";
+        String expected = Files.readString(Path.of("src/test/resources/fixtures/result_json.txt"));
         assertEquals(expected, Differ.generate(json1, json2, "json"));
     }
 
     @Test
+    public void testYamlToStylish() throws IOException {
+        String json1 = "src/test/resources/fixtures/file1.yml";
+        String json2 = "src/test/resources/fixtures/file2.yml";
+        String expected = Files.readString(Path.of("src/test/resources/fixtures/result_stylish.txt"));
+        assertEquals(expected, Differ.generate(json1, json2, "stylish"));
+    }
+    @Test
+    public void testYamlToPlain() throws IOException {
+        String json1 = "src/test/resources/fixtures/file1.yml";
+        String json2 = "src/test/resources/fixtures/file2.yml";
+        String expected = Files.readString(Path.of("src/test/resources/fixtures/result_plain.txt"));
+        assertEquals(expected, Differ.generate(json1, json2, "plain"));
+    }
+    @Test
+    public void testYamlToJson() throws IOException {
+        String json1 = "src/test/resources/fixtures/file1.yml";
+        String json2 = "src/test/resources/fixtures/file2.yml";
+        String expected = Files.readString(Path.of("src/test/resources/fixtures/result_json.txt"));
+        assertEquals(expected, Differ.generate(json1, json2, "json"));
+    }
+
+    @Test
+    public void testJsonTwoParams() throws IOException {
+        String json1 = "src/test/resources/fixtures/file1.json";
+        String json2 = "src/test/resources/fixtures/file2.json";
+        String expected = Files.readString(Path.of("src/test/resources/fixtures/result_stylish.txt"));
+        assertEquals(expected, Differ.generate(json1, json2));
+    }
+
+    @Test
+    public void testYamlTwoParams() throws IOException {
+        String json1 = "src/test/resources/fixtures/file1.json";
+        String json2 = "src/test/resources/fixtures/file2.json";
+        String expected = Files.readString(Path.of("src/test/resources/fixtures/result_stylish.txt"));
+        assertEquals(expected, Differ.generate(json1, json2));
+    }
+
+    @Test
     public void testYamlParserOn() {
-        String tsrc = "src/test/resources/file1.yml";
+        String tsrc = "src/test/resources/fixtures/file1.yml";
         String expected = YAMLMapper.class.getSimpleName();
         assertEquals(expected, Parser.getMapper(tsrc).getClass().getSimpleName());
     }
